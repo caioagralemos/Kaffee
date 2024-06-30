1b399cc8c8c8cfc7c013a721e9201053d6029a8f
Caio <caioagralemos@live.com>
2024-06-30 16:12:18 -0300

    Initial Commit

---
+++ Kaffee/KaffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaffeeApp.swift
+//  Kaffee
+//
+//  Created by Caio on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct KaffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
